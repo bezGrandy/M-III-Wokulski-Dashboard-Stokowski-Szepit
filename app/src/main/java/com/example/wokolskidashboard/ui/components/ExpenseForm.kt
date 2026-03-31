0be0fc3a3be6cf7716d4e5c2a596a1888491aca7
wojteks <wojciech.stokowski@cyfroweszkoly.pl>
2026-03-31 09:24:55 +0200

    create ExpenseForm and add expense name field

--- app/src/main/java/com/example/wokolskidashboard/ui/components/ExpenseForm.kt
+++ app/src/main/java/com/example/wokolskidashboard/ui/components/ExpenseForm.kt
@@ -1,2 +1,42 @@
 package com.example.wokolskidashboard.ui.components
 
+import android.R.attr.name
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ExpenseForm(
+    onAddExpense: (name: String, amount: Double, category: String, isOptional: Boolean) -> Unit
+) {
+    var name by remember { mutableStateOf("") }
+
+
+    Column(modifier = Modifier.padding(16.dp)) {
+
+        Text(text = "Dodaj wydatek", style = MaterialTheme.typography.titleMedium)
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = name,
+            onValueChange = { name = it },
+            label = { Text("Nazwa") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+        }
+    }
