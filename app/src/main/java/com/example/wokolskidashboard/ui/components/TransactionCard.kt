5560ead37e9d2a5ec76f9bd1cba06d307fb626f1
bezGrandy <wojciech.stokowski@cyfroweszkoly.pl>
2026-04-25 23:16:42 +0200

    add transaction list

--- app/src/main/java/com/example/wokolskidashboard/ui/components/TransactionCard.kt
+++ app/src/main/java/com/example/wokolskidashboard/ui/components/TransactionCard.kt
@@ -1,2 +1,43 @@
 package com.example.wokolskidashboard.ui.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.wokolskidashboard.model.Transaction
+
+@Composable
+fun TransactionCard(transaction: Transaction) {
+
+    val color = if (transaction.isExpense)
+        MaterialTheme.colorScheme.error
+    else
+        MaterialTheme.colorScheme.primary
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = transaction.name)
+
+            Text(
+                text = if (transaction.isExpense)
+                    "-${transaction.amount}"
+                else
+                    "+${transaction.amount}",
+                color = color
+            )
+        }
+    }
+}
