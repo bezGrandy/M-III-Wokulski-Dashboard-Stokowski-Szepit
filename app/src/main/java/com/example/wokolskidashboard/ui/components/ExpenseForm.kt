1857213b90898275b061d6af82619fbdfc26399c
bezGrandy <wojciech.stokowski@cyfroweszkoly.pl>
2026-04-25 16:32:04 +0200

    tweak ExpenseForm UI

--- app/src/main/java/com/example/wokolskidashboard/ui/components/ExpenseForm.kt
+++ app/src/main/java/com/example/wokolskidashboard/ui/components/ExpenseForm.kt
@@ -1,39 +1,29 @@
 package com.example.wokolskidashboard.ui.components
-
-import android.R.attr.name
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun ExpenseForm(
-    onAddExpense: (name: String, amount: Double, category: String, isOptional: Boolean) -> Unit
+    onAddExpense: (String, Double, String, Boolean) -> Unit
 ) {
     var name by remember { mutableStateOf("") }
     var amount by remember { mutableStateOf("") }
+    var category by remember { mutableStateOf("Wydatki Osobiste") }
+    var isNecessary by remember { mutableStateOf(true) }
 
     Column(modifier = Modifier.padding(16.dp)) {
 
-        Text(text = "Dodaj wydatek", style = MaterialTheme.typography.titleMedium)
+        Text("Dodaj Wydatek", style = MaterialTheme.typography.titleLarge)
 
         Spacer(modifier = Modifier.height(8.dp))
 
         OutlinedTextField(
             value = name,
             onValueChange = { name = it },
-            label = { Text("Nazwa") },
+            label = { Text("Cel wydatku") },
             modifier = Modifier.fillMaxWidth()
         )
 
@@ -45,7 +35,5 @@ fun ExpenseForm(
             label = { Text("Kwota") },
             modifier = Modifier.fillMaxWidth()
         )
-
-        Spacer(modifier = Modifier.height(8.dp))
     }
 }
