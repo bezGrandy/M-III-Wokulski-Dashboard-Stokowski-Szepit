851eb27ea355cfaff8a6f3eb079903752ead45d5
bezGrandy <wojciech.stokowski@cyfroweszkoly.pl>
2026-04-25 17:09:31 +0200

    add state hoisting for expenses

--- app/src/main/java/com/example/wokolskidashboard/ui/MainScreen.kt
+++ app/src/main/java/com/example/wokolskidashboard/ui/MainScreen.kt
@@ -1,2 +1,13 @@
 package com.example.wokolskidashboard.ui
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import com.example.wokolskidashboard.model.Transaction
+
+@Composable
+fun MainScreen() {
+
+    val transactions = remember { mutableStateListOf<Transaction>() }
+
+}
